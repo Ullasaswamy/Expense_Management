81c6fa30877788640d9d6bbc7eabd260fa5ccbb0
prasanth.a <prashanth.a@ankiom.com>
2026-02-24 14:51:10 +0530

    api changes

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ const db = require('./db');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const helmet = require('helmet');
+app.use(helmet());
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,13 +16,12 @@ app.set('trust proxy', 1);
 
 const corsOptions = {
     origin: process.env.FRONTEND_URL,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 
 
 app.use(express.json());
